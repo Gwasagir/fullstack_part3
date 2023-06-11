513b692bf6b94a36a369bfe3cb4b833af9405395
Niko Keurulainen <keurulainen.niko@gmail.com>
2023-06-11 12:28:40 +0300

    Started 3.2, response.Date not working yet

--- index.js
+++ index.js
@@ -3,31 +3,48 @@ const app = express()
 
 app.use(express.json())
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
 ]
 
+const countEntries = entries => {
+    const personcount = entries.reduce(function(sum){
+        return sum + 1
+    },0)
+    return personcount
+}
 
-app.get('/', (request, response) => {
-    response.send('<h2>You can find phonebook entires from /api/persons</h2>')
+app.get('/info', (request, response) => {
+    const entries = countEntries(persons)
+    console.log(request)
+    const requestDate = response.Date
+    console.log(requestDate)
+    response.send(
+        '<p>Phonebook has info for '+entries+' people</p><p> '+
+        requestDate + '</p>')
   })
   
 app.get('/api/persons', (request, response) => {
-    response.json(notes)
+    response.json(persons)
   })
 
 const PORT = 3001
